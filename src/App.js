317d0104457d8c5c589c55aecd5b706a2fb19839
yasin <ya30nziya@gmail.com>
2021-12-13 11:16:36 -0800

    frant end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { useEffect } from "react";
 function App() {
+  let [list, setList] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/products")
+      .then((response) => response.json())
+      .then((response) => {
+        setList(response);
+      });
+  }, []);
+  console.log(list);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {list.map((Item) => (
+        <h1 key={Item}>{Item}</h1>
+      ))}
     </div>
   );
 }
-
 export default App;
